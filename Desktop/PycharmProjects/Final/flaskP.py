ced7c8d10de9358b70d53caee4c46e55fe34f1b6
Daniel Grbin <dan.grbin1@seznam.cz>
2018-08-17 18:38:45 +0200

    fix

--- Desktop/PycharmProjects/Final/flaskP.py
+++ Desktop/PycharmProjects/Final/flaskP.py
@@ -1,10 +1,10 @@
-from flask import Flask
+﻿from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Ahoj světe..!'
+    return 'Hello World!'
 
 
 @app.route('/cs')
